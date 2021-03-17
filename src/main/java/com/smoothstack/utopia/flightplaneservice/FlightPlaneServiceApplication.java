45c0a80f460246b866976adc400822503d7092bd
Rob Maes <robert.maes@smoothstack.com>
2021-03-16 23:11:24 -0500

    initial commit

---
+++ src/main/java/com/smoothstack/utopia/flightplaneservice/FlightPlaneServiceApplication.java
@@ -0,0 +1,13 @@
+package com.smoothstack.utopia.flightplaneservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightPlaneServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FlightPlaneServiceApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/smoothstack/utopia/flightplaneservice/FlightPlaneServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smoothstack.utopia.flightplaneservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightPlaneServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
