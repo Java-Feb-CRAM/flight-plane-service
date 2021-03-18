5d0f031fa3ed6e49c71909dd7eebadf0ef5198db
Rob Maes <robert.maes@smoothstack.com>
2021-03-18 11:12:59 -0500

    implement createFlight method

--- src/main/java/com/smoothstack/utopia/flightplaneservice/dto/CreateFlightDto.java
+++ src/main/java/com/smoothstack/utopia/flightplaneservice/dto/CreateFlightDto.java
@@ -1,7 +1,38 @@
 package com.smoothstack.utopia.flightplaneservice.dto;
 
+import java.time.Instant;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.PositiveOrZero;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
 /**
  * @author Rob Maes
  * Mar 18 2021
  */
-public class CreateFlightDto {}
+@Getter
+@Setter
+@ToString
+public class CreateFlightDto {
+
+  @NotNull(message = "Route ID is required")
+  @Positive(message = "Route ID must be a positive integer")
+  private Long routeId;
+
+  @NotNull(message = "Airplane ID is required")
+  @Positive(message = "Airplane ID must be a positive integer")
+  private Long airplaneId;
+
+  @NotNull(message = "Departure time is required")
+  private Instant departureTime;
+
+  @NotNull(message = "Number of reserved seats is required")
+  @PositiveOrZero(message = "Number of reserved seats must be 0 or greater")
+  private Integer reservedSeats;
+
+  @NotNull(message = "Seat price is required")
+  @PositiveOrZero(message = "Seat price must be 0 or greater")
+  private Float seatPrice;
+}
--- src/main/java/com/smoothstack/utopia/flightplaneservice/service/FlightService.java
+++ src/main/java/com/smoothstack/utopia/flightplaneservice/service/FlightService.java
@@ -1,10 +1,16 @@
 package com.smoothstack.utopia.flightplaneservice.service;
 
+import com.smoothstack.utopia.flightplaneservice.dao.AirplaneDao;
 import com.smoothstack.utopia.flightplaneservice.dao.FlightDao;
+import com.smoothstack.utopia.flightplaneservice.dao.RouteDao;
 import com.smoothstack.utopia.flightplaneservice.dto.CreateFlightDto;
 import com.smoothstack.utopia.flightplaneservice.dto.UpdateFlightDto;
+import com.smoothstack.utopia.flightplaneservice.exception.AirplaneNotFoundException;
 import com.smoothstack.utopia.flightplaneservice.exception.FlightNotFoundException;
+import com.smoothstack.utopia.flightplaneservice.exception.RouteNotFoundException;
+import com.smoothstack.utopia.shared.model.Airplane;
 import com.smoothstack.utopia.shared.model.Flight;
+import com.smoothstack.utopia.shared.model.Route;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,10 +23,18 @@ import org.springframework.stereotype.Service;
 public class FlightService {
 
   private final FlightDao flightDao;
+  private final RouteDao routeDao;
+  private final AirplaneDao airplaneDao;
 
   @Autowired
-  public FlightService(FlightDao flightDao) {
+  public FlightService(
+    FlightDao flightDao,
+    RouteDao routeDao,
+    AirplaneDao airplaneDao
+  ) {
     this.flightDao = flightDao;
+    this.routeDao = routeDao;
+    this.airplaneDao = airplaneDao;
   }
 
   public List<Flight> getAllFlights() {
@@ -33,7 +47,22 @@ public class FlightService {
       .orElseThrow(FlightNotFoundException::new);
   }
 
-  public void createFlight(CreateFlightDto createFlightDto) {}
+  public void createFlight(CreateFlightDto createFlightDto) {
+    Route route = routeDao
+      .findById(createFlightDto.getRouteId())
+      .orElseThrow(RouteNotFoundException::new);
+    Airplane airplane = airplaneDao
+      .findById(createFlightDto.getAirplaneId())
+      .orElseThrow(AirplaneNotFoundException::new);
+    Flight flight = new Flight(
+      route,
+      airplane,
+      createFlightDto.getDepartureTime(),
+      createFlightDto.getReservedSeats(),
+      createFlightDto.getSeatPrice()
+    );
+    flightDao.save(flight);
+  }
 
   public void updateFlight(Long flightId, UpdateFlightDto updateFlightDto) {}
 
