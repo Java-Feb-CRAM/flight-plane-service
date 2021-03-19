814bfbd1acdeca2715c43a14ff848dc993597524
Rob Maes <robert.maes@smoothstack.com>
2021-03-18 21:29:03 -0500

    ut-24 support xml and use env vars for db credentials

--- src/main/java/com/smoothstack/utopia/flightplaneservice/controller/AirportController.java
+++ src/main/java/com/smoothstack/utopia/flightplaneservice/controller/AirportController.java
@@ -9,6 +9,7 @@ import javax.validation.Valid;
 import javax.ws.rs.Path;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -34,7 +35,11 @@ public class AirportController {
     this.airportService = airportService;
   }
 
-  @GetMapping
+  @GetMapping(
+    produces = {
+      MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE,
+    }
+  )
   public List<Airport> getAllAirports() {
     return airportService.getAllAirports();
   }
--- src/main/java/com/smoothstack/utopia/flightplaneservice/dto/CreateAirplaneDto.java
+++ src/main/java/com/smoothstack/utopia/flightplaneservice/dto/CreateAirplaneDto.java
@@ -1,7 +1,32 @@
 package com.smoothstack.utopia.flightplaneservice.dto;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
 /**
  * @author Rob Maes
  * Mar 18 2021
  */
-public class CreateAirplaneDto {}
+@Getter
+@Setter
+public class CreateAirplaneDto {
+
+  @NotBlank(message = "Airport ID is required")
+  @Size(
+    min = 3,
+    max = 3,
+    message = "Airport ID must be exactly 3 characters in length"
+  )
+  @Pattern(
+    regexp = "[A-Z]{3}",
+    message = "Airport ID can only contain capital letters"
+  )
+  private String iataId;
+
+  @NotBlank(message = "Airport city is required")
+  @Size(max = 255, message = "Airport city must not exceed 255 characters")
+  private String city;
+}
