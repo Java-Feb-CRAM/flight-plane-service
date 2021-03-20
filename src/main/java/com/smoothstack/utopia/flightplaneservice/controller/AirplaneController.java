75720e6bfe99ec49cfde28ffcfaf709e25ad09a3
Rob Maes <robert.maes@smoothstack.com>
2021-03-20 14:26:25 -0500

    ut-24 allow CORS * for frontend testing

--- src/main/java/com/smoothstack/utopia/flightplaneservice/controller/AirplaneController.java
+++ src/main/java/com/smoothstack/utopia/flightplaneservice/controller/AirplaneController.java
@@ -9,6 +9,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,6 +24,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author Rob Maes
  * Mar 18 2021
  */
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping(
   path = "/airplanes",
